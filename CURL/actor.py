97eb3f7f4e816f13d2ff4c20163684ba4abc6c2e
Xu Kang <xukang_0317@163.com>
2022-03-20 10:45:41 +0800

    change log_prob computation

--- CURL/actor.py
+++ CURL/actor.py
@@ -36,10 +36,10 @@ class Actor(nn.Module):
     def __call__(self, obs: torch.tensor, detach_encoder: bool, compute_pi: bool , compute_log_prob: bool) -> Tuple:
         x = self.encoder(obs, detach_encoder)
 
-        mu, arctanh_log_std = self.trunk(x).chunk(2, dim=-1)
+        mu, log_std = self.trunk(x).chunk(2, dim=-1)
 
-        log_std = torch.clamp(arctanh_log_std, self.logstd_min, self.logstd_max)
-        log_std = torch.tanh(arctanh_log_std)
+        log_std = torch.clamp(log_std, self.logstd_min, self.logstd_max)
+        #log_std = torch.tanh(log_std)
         #log_std = self.logstd_min + 0.5 * (self.logstd_max - self.logstd_min) * (log_std + 1)
 
         self.outputs['mu'] = mu
